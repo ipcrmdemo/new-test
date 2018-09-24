6335f27f1c94d0437e611eaaa1d717660578de4f
Matt Cadorette <mcadorette@gmail.com>
2018-09-24 13:24:06 -0400

    Initial commit from Atomist

---
+++ src/main/java/newtest/NewTestApplication.java
@@ -0,0 +1,12 @@
+package newtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTestApplication.class, args);
+	}
+}
---
+++ src/main/java/newtest/NewTestController.java
@@ -0,0 +1,20 @@
+package newtest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class NewTestController {
+
+    @GetMapping(path = "hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping(path = "/")
+    public String root() {
+        return "Hello, world! Add /hello/there to the URL to get a friendly reply.";
+    }
+
+}
